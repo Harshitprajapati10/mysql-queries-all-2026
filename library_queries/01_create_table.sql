6ed0f3c3ac0d8f7ef0888a5149e47e1d65578907
Harshit Prajapati <143312448+Harshitprajapati10@users.noreply.github.com>
2026-05-10 12:25:42 +0000

    table created successfully

---
+++ library_queries/01_create_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE combinedlibrary (
+    acession_no BIGINT,
+    book_title TEXT,
+    author TEXT,
+    price_inr VARCHAR(50),
+    price_dollar VARCHAR(50),
+    price_euro VARCHAR(50),
+    publisher TEXT,
+    issued VARCHAR(50),
+    number_donated INT,
+    book_shelf_no VARCHAR(50),
+    compartment_no VARCHAR(50),
+    no_copies INT
+);
---
+++ queries/09_new_db_create.sql
@@ -0,0 +1,3 @@
+-- Active: 1778316798577@@turntable.proxy.rlwy.net@53042@university_db
+
+CREATE DATABASE combinedlibrary;
